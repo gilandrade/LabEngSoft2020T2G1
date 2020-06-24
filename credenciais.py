a0e0b67d2bf96a62173be7a02b231426a34b72db
gilandrade <38143239+gilandrade@users.noreply.github.com>
2020-06-24 16:10:24 -0300

    Export .json file

--- credenciais.py
+++ credenciais.py
@@ -2,7 +2,9 @@ import fitbit
 import gather_keys_oauth2 as Oauth2
 import pandas as pd 
 import datetime
+from datetime import date
 import os
+import json
 CLIENT_ID = '22BMBD'
 CLIENT_SECRET = '41bf164e37513442e8fc5c3f36f3d876'
 
@@ -14,16 +16,26 @@ REFRESH_TOKEN=str(server.fitbit.client.session.token['refresh_token'])
 auth2_client=fitbit.Fitbit(CLIENT_ID,CLIENT_SECRET,oauth2=True,access_token=ACCESS_TOKEN,refresh_token=REFRESH_TOKEN)
 
 #Definindo datas
-#year = input('Insira o ano: ')
-#month = input('Insira o mês: ')
-#day = input('Insira o dia: ')
-data = str(datetime.datetime.today().strftime("%Y-%m-%d"))
+'''
+year = input('Insira o ano: ')
+month = input('Insira o mês: ')
+day = input('Insira o dia: ')
+'''
+data = str(date.today().strftime("%Y-%m-%d"))
 
 
 #Chamando dados
 oneDayData = auth2_client.time_series('activities/steps', base_date=data, period = '30d')
+'''d = json.dumps(oneDayData)
+print(d)'''
 
-#Salvando em planilha
+#Criando o arquivo .json
+with open('Passos '+ data +'.json', 'w') as f:
+    json.dump(oneDayData, f)
+
+
+#Salvando em planilha excel
+'''
 time_list = []
 val_list = []
 for i in oneDayData['activities-steps']:
@@ -33,3 +45,4 @@ df = pd.DataFrame({'Passos':val_list,'Dia':time_list})
 filename = oneDayData['activities-steps'][0]['dateTime']
 path = os.path.join(os.path.expanduser("~"), "Desktop/")
 df.to_excel(path + 'Passos ' + filename + '.xlsx', columns=['Passos','Dia'], header = True, index = False)
+'''
